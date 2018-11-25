0837814257cee584aac7c75d73651ff9fdffa4d9
renjithspace <renjithrajeevvk@gmail.com>
2018-11-25 20:28:01 +0530

    Init anchor scroll

--- src/app.js
+++ src/app.js
@@ -1,5 +1,12 @@
 import 'bootstrap'
 import 'animate.css'
 const wow = require('wowjs')
+const anchorScroll = require("anchor-scroll")
+
+anchorScroll.init({
+  updateUrl: false,
+  offset: 0,
+  duration: 1000
+})
 
 new wow.WOW().init()
