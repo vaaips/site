e0203e47b2dfcf92eed7bca9e7136893ebafe641
renjithspace <renjithrajeevvk@gmail.com>
2018-12-19 15:15:26 +0530

    Send the get quote form mail

--- api/server.js
+++ api/server.js
@@ -1,6 +1,7 @@
 const Joi = require('joi')
 const bodyParser = require('body-parser')
 const _ = require('lodash')
+const SparkPost = require('sparkpost')
 
 const apiServer = app => {
   app.use(bodyParser.urlencoded({ extended: false }))
@@ -21,8 +22,35 @@ const apiServer = app => {
       const error = _.head(validate.error.details).message
       return res.status(422).json(error)
     }
+
+    const client = new SparkPost(process.env.SPARKPOST_API_KEY)
+    const send = client.transmissions.send({
+      options: {
+        sandbox: true
+      },
+      content: {
+        from: data.email,
+        subject: `${data.name} Requested for Quote - Vaaip`,
+        attachments: data.attachment ? data.attachment : null,
+        html: `
+          <html>
+            <body>
+              <p>${data.name} (${data.email})</p>
+              <p>
+                ${data.message}<br>
+                Budget: ${data.budget}<br>
+                Platforms: data.platforms
+              </p>
+            </body>
+          </html>`
+      },
+      recipients: [
+        { address: process.env.EMAIL }
+      ]
+    })
     
-    res.json(true)
+    send.then(data => res.json('Thanks! We\'ll get back to you soon'))
+      .catch(error => res.status(403).json('Whoops! Something went wrong'))
   })
 }
 
