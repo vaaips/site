10f12fa353785d494d4fe1738d7258f2bf076a59
renjithspace <renjithrajeevvk@gmail.com>
2018-12-20 11:03:11 +0530

    Create packager

---
+++ packager.sh
@@ -0,0 +1,17 @@
+echo Cleanig up 🚿
+rm -rf package
+mkdir package
+
+echo Copying files 💥
+cp -r api package/api
+cp -r client package/client
+cp .env.example package/.env
+cp app.js package/app.js
+cp package.json package/package.json
+cp package-lock.json package/package-lock.json
+
+echo Finishing 🍻
+rm -rf package/client/.cache
+rm -rf package/client/dist
+
+echo Package is ready for serve 🍻
