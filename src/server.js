2ab6f80c469b6dcb9870f80957835c0f63721333
renjithspace <renjithrajeevvk@gmail.com>
2019-11-04 10:21:50 +0530

    Remove

--- src/server.js
+++
@@ -1,9 +0,0 @@
-const path = require('path')
-const express = require('express')
-
-const clientServer = app => {
-  const buildPath = path.join(__dirname, './dist')
-  app.use(express.static(buildPath))
-}
-
-module.exports = clientServer
