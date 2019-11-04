a4fa984deea7f679db08469e4c844e64f4dac8fe
renjithspace <renjithrajeevvk@gmail.com>
2019-11-04 10:20:59 +0530

    Remove listen console log

--- app.js
+++ app.js
@@ -5,4 +5,4 @@ const port = process.env.SERVER_PORT
 const publicPath = path.join(__dirname, './dist')
 
 app.use(express.static(publicPath))
-app.listen(port, () => console.log(`Serving on port ${port}`))
+app.listen(port)
